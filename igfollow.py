55f35da347dc177da7f2cba99708bc1bc2733558
FatGuyy <omkarjadhav2103@gmail.com>
2022-06-20 15:49:53 +0530

    uploading main file

--- igfollow.py
+++ igfollow.py
@@ -1,8 +1,10 @@
+from lib2to3.pgen2.driver import Driver
 from logging import exception
 import time
 import os 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 
 
 try:
@@ -35,18 +37,28 @@ try:
     
     profile = driver.find_element_by_xpath('/html/body/div[1]/section/nav/div[2]/div/div/div[2]/div[3]/div/div[2]/div/div[1]/a')
     profile.click()
-    #Getting the 
+    
+    followers_count = driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[1]/div/div/div[1]/div[1]/section/main/div/header/section/ul/li[2]/a/div/span')
+    followers_count = int(followers_count.text)
     followers_button = driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[1]/div/div/div[1]/div[1]/section/main/div/header/section/ul/li[2]/a/div')
     followers_button.click()
-        
-   
+    
+    #Gets the name of follower
+    #Xpath for the names = /html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div/div[2]/ul/div
+    #follower = driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div/div[2]/ul/div/li[1]/div/div[2]/div[1]/div/div/span')
+    #print(follower.text)
+    time.sleep(4)
+    #driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+
+
+    print('Till here')
+    element = driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div/div[2]')
+    print('Till loop')
+    element = driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div/div[2]/ul/div/li[100]")
+    driver.execute_script("return arguments[0].scrollIntoView();", element)
     #laughing__soul__
     
 
-    #followers_button = driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[1]/div/div/div[1]/div[1]/section/main/div/header/section/ul/li[2]/a/div')
-    #followers_button.click()
-
-
 finally:
     time.sleep(10)
-    #driver.close()
+    driver.close()
